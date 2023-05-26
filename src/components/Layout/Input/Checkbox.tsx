b82d53b2c0de0d9f806a7e2a802a9707fc8132dc
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-05-26 21:55:47 +0200

    App

--- src/components/Layout/Input/Checkbox.tsx
+++ src/components/Layout/Input/Checkbox.tsx
@@ -4,39 +4,52 @@ import React, { useState } from "react";
 import * as CheckBox from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
 
-type CheckboxProps = { darkMode: boolean; checkboxLabel: string };
+type CheckboxProps = {
+  darkMode: boolean;
+  checkboxLabel: string;
+  subTaskId: number;
+  isCompleted: boolean;
+  toggleSubtask: (subTaskId: number) => void;
+};
 
-const Checkbox: React.FC<CheckboxProps> = ({ darkMode, checkboxLabel }) => {
+const Checkbox: React.FC<CheckboxProps> = ({
+  darkMode,
+  checkboxLabel,
+  subTaskId,
+  isCompleted,
+  toggleSubtask,
+}) => {
   const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <form>
-      <div
-        className={`flex items-center justify-start bg-veryDarkGrey 
+    <div
+      className={`flex items-center justify-start bg-veryDarkGrey 
        hover:bg-[rgba(99,_95,_199,_.25)] text-400 py-3 px-3 mb-2 ${
          darkMode ? "bg-veryDarkGrey text-white" : "bg-white text-black"
        }`}
+    >
+      <CheckBox.Root
+        className={`h-4 aspect-square border-[1px] border-[rgba(130,_143,_163,_0.24)] rounded-sm bg-white `}
+        checked={isCompleted}
+        // onClick={() => setIsClicked((prev) => !prev)}
+        onClick={() => {
+          toggleSubtask(subTaskId);
+        }}
+        id={checkboxLabel}
       >
-        <CheckBox.Root
-          className={`h-4 aspect-square border-[1px] border-[rgba(130,_143,_163,_0.24)] rounded-sm bg-white `}
-          checked={isClicked}
-          onClick={() => setIsClicked((prev) => !prev)}
-          id={checkboxLabel}
-        >
-          <CheckBox.Indicator className="">
-            <CheckIcon className="text-white bg-purple h-4" />
-          </CheckBox.Indicator>
-        </CheckBox.Root>
-        <label
-          className={`text-bold px-4 cursor-pointer ${
-            isClicked && "line-through opacity-50"
-          }`}
-          htmlFor={checkboxLabel}
-        >
-          {checkboxLabel}
-        </label>
-      </div>
-    </form>
+        <CheckBox.Indicator>
+          <CheckIcon className="text-white bg-purple h-4" />
+        </CheckBox.Indicator>
+      </CheckBox.Root>
+      <label
+        className={`text-bold px-4 cursor-pointer ${
+          isClicked && "line-through opacity-50"
+        }`}
+        htmlFor={checkboxLabel}
+      >
+        {checkboxLabel}
+      </label>
+    </div>
   );
 };
 export default Checkbox;
--- src/components/Modal/Task/ViewTaskModal.tsx
+++ src/components/Modal/Task/ViewTaskModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import Checkbox from "../../Layout/Input/Checkbox";
@@ -7,17 +7,18 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { modalState } from "@/src/atoms/modalAtom";
 import { settingsModalState } from "../../../atoms/settingsModalAtom";
 import { boardsState } from "../../../atoms/boardsAtom";
-import { TaskType } from "../../Board/BoardType";
+import { SubtasksType, TaskType } from "../../Board/BoardType";
 
 type ViewTaskModalProps = { darkMode: boolean };
 
 const ViewTaskModal: React.FC<ViewTaskModalProps> = ({ darkMode }) => {
   const [modalsState, setModalsState] = useRecoilState(modalState);
   const [settingState, setSettingState] = useRecoilState(settingsModalState);
-
+  const [loading, setLoading] = useState<boolean>(true);
   const [boardState, setBoardState] = useRecoilState(boardsState);
 
   const [activatedTask, setActivatedTask] = useState<TaskType>();
+  const [completedTask, setCompletedTask] = useState<number>(0);
 
   const sprawdz = () => {
     // console.log(settingState);
@@ -27,9 +28,83 @@ const ViewTaskModal: React.FC<ViewTaskModalProps> = ({ darkMode }) => {
     const activatedColumn = currentBoard?.columns.find(
       (item) => item.id === settingState.activateColumn
     );
-    console.log(activatedColumn);
     // const currentTask = currentBoard?.columns.
+    setActivatedTask(
+      activatedColumn?.tasks.find(
+        (item) => item.id === settingState.activateTask
+      )
+    );
+    setActivatedTask(activatedTask);
+    console.log(activatedTask);
+    setCompletedTask(
+      activatedTask!.subtasks.filter((item) => item.isCompleted).length
+    );
   };
+  useEffect(() => {
+    if (loading) {
+      const currentBoard = boardState.find(
+        (item) => item.name === settingState.activeBoard
+      );
+      const activatedColumn = currentBoard?.columns.find(
+        (item) => item.id === settingState.activateColumn
+      );
+      // const currentTask = currentBoard?.columns.
+      setActivatedTask(
+        activatedColumn?.tasks.find(
+          (item) => item.id === settingState.activateTask
+        )
+      );
+      setLoading(false);
+    }
+    // setActivatedTask(activatedTask);
+    // console.log(activatedTask?.title);
+    setCompletedTask(
+      activatedTask!.subtasks.filter((item) => item.isCompleted).length
+    );
+  }, [
+    activatedTask,
+    boardState,
+    loading,
+    settingState.activateColumn,
+    settingState.activateTask,
+    settingState.activeBoard,
+  ]);
+  useEffect(() => {
+    if (modalsState.open) setLoading(true);
+  }, [modalsState]);
+  const toggleSubtask = (subTaskId: number) => {
+    const updatedSubtask = activatedTask?.subtasks.map((item) =>
+      item.id === subTaskId ? { ...item, isCompleted: !item.isCompleted } : item
+    );
+    // console.log(updatedSubtask);
+    console.log(activatedTask?.subtasks);
+    console.log(updatedSubtask);
+
+    setActivatedTask((prev) => {
+      return { ...prev, subtasks: updatedSubtask };
+    });
+    // setActivatedTask({
+    //   description: "",
+    //   id: 11,
+    //   status: "",
+    //   subtasks: updatedSubtask,
+    //   title: "",
+    // });
+    // console.log(tests);
+
+    // console.log(activatedTask?.subtasks[0]);
+  };
+
+  const checkBox = activatedTask?.subtasks.map((item) => (
+    <Checkbox
+      key={item.id}
+      darkMode={darkMode}
+      checkboxLabel={item.title}
+      subTaskId={item.id}
+      isCompleted={item.isCompleted}
+      toggleSubtask={toggleSubtask}
+    />
+  ));
   return (
     <Dialog.Portal>
       <Dialog.Overlay
@@ -44,51 +119,34 @@ const ViewTaskModal: React.FC<ViewTaskModalProps> = ({ darkMode }) => {
          }
           p-[25px] 
           shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]
-          focus:outline-none`}>
+          focus:outline-none`}
+      >
         <Dialog.Title
-          className={` ${darkMode ? "text-white" : "text-black"} text-800`}>
+          className={` ${darkMode ? "text-white" : "text-black"} text-800`}
+        >
           <div className="flex items-center">
-            <p>
-              {" "}
-              Research pricing points of various competitors and trial different
-              business models
-            </p>
-            {/* <BiDotsVerticalRounded
-              className="text-[3.5rem] text-mediumGrey cursor-pointer"
-              onClick={() =>
-                setModalsState((prev) => ({ ...prev, view: "editTask" }))
-              }
-            /> */}
+            <p> {activatedTask?.title}</p>
           </div>
         </Dialog.Title>
         <Dialog.Description className="text-mediumGrey my-[1.5rem] text-500">
-          We know what we`re planning to build for version one. Now we need to
-          finalise the first pricing model we`ll use. Keep iterating the
-          subtasks until we have a coherent proposition.
+          {activatedTask?.description}
         </Dialog.Description>
         <Dialog.Description
           className={` pb-4 text-500 ${
             darkMode ? "text-white" : "text-mediumGrey"
-          }`}>
-          Subtasks (2 of 3)
+          }`}
+        >
+          Subtasks ({completedTask} of {activatedTask?.subtasks.length})
         </Dialog.Description>
-        <Checkbox
-          checkboxLabel="Research competitor pricing and business models"
-          darkMode={darkMode}
-        />
-        <Checkbox
-          checkboxLabel="Outline a business model that works for our solution"
-          darkMode={darkMode}
-        />
-        <Checkbox checkboxLabel="Surveying and testing" darkMode={darkMode} />
+        {checkBox}
         <p
           className={` text-400 pt-4 pb-2 ${
             darkMode ? "text-white" : "text-mediumGrey"
-          }`}>
+          }`}
+        >
           Current Status
         </p>
         <button onClick={sprawdz}>kkkkk</button>
-        {/* <DropMenu darkMode={darkMode} /> */}
       </Dialog.Content>
     </Dialog.Portal>
   );
