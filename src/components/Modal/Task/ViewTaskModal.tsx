831d37dc5d5932b72f5ab2d7b5a095b3b9db1f06
kamiliano1 <kamiliano1991@gmail.com>
2023-05-28 09:14:35 +0200

    Update ViewTaskModal.tsx

--- src/components/Modal/Task/ViewTaskModal.tsx
+++ src/components/Modal/Task/ViewTaskModal.tsx
@@ -7,62 +7,88 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { modalState } from "@/src/atoms/modalAtom";
 import { settingsModalState } from "../../../atoms/settingsModalAtom";
 import { boardsState } from "../../../atoms/boardsAtom";
-import { SubtasksType, TaskType } from "../../Board/BoardType";
+import {
+  BoardType,
+  ColumnType,
+  SubtasksType,
+  TaskType,
+} from "../../Board/BoardType";
+import { Controller, useForm } from "react-hook-form";
 
 type ViewTaskModalProps = { darkMode: boolean };
-
+interface BoardInputs {
+  title: string;
+  description: string;
+  subtasks: SubtasksType[];
+  status: string;
+}
 const ViewTaskModal: React.FC<ViewTaskModalProps> = ({ darkMode }) => {
   const [modalsState, setModalsState] = useRecoilState(modalState);
   const [settingState, setSettingState] = useRecoilState(settingsModalState);
   const [loading, setLoading] = useState<boolean>(true);
   const [boardState, setBoardState] = useRecoilState(boardsState);
+  const [currentBoard, setCurrentBoard] = useState<BoardType>();
+  const [currrentColumn, setCurrentColumn] = useState<ColumnType>();
+  const [currentTask, setCurrentTask] = useState<TaskType>();
+  const [completedTask, setCompletedTask] = useState<number | undefined>(0);
+  const [columnsName, setColumnsName] = useState<string[]>([]);
 
-  const [activatedTask, setActivatedTask] = useState<TaskType>();
-  const [completedTask, setCompletedTask] = useState<number>(0);
-
+  const { control, getValues, reset } = useForm<BoardInputs>();
   const sprawdz = () => {
     // console.log(settingState);
+    const currentBoard = boardState.find(
+      (item) => item.name === settingState.activeBoard
+    );
+    setCurrentBoard(
+      boardState.find((item) => item.name === settingState.activeBoard)
+    );
+    setCurrentColumn(
+      currentBoard?.columns.find(
+        (item) => item.id === settingState.activateColumn
+      )
+    );
+    // const activatedColumn = currentBoard?.columns.find(
+    //   (item) => item.id === settingState.activateColumn
+    // );
+    // const currentTask = currentBoard?.columns.
+    setCurrentTask(
+      currrentColumn?.tasks.find(
+        (item) => item.id === settingState.activateTask
+      )
+    );
+    setCurrentTask(currentTask);
+    console.log(currentTask);
+    setCompletedTask(
+      currentTask?.subtasks.filter((item) => item.isCompleted).length
+    );
+
+    console.log(getValues("status"), "wartoscki");
+  };
+  useEffect(() => {
+    // if (loading) {
     const currentBoard = boardState.find(
       (item) => item.name === settingState.activeBoard
     );
     const activatedColumn = currentBoard?.columns.find(
       (item) => item.id === settingState.activateColumn
     );
+    if (currentBoard)
+      setColumnsName(currentBoard.columns.map((item) => item.name));
     // const currentTask = currentBoard?.columns.
-    setActivatedTask(
+    setCurrentTask(
       activatedColumn?.tasks.find(
         (item) => item.id === settingState.activateTask
       )
     );
-    setActivatedTask(activatedTask);
-    console.log(activatedTask);
+    setLoading(false);
+    // }
+    // setCurrentTask(currentTask);
+    // console.log(currentTask?.title);
     setCompletedTask(
-      activatedTask!.subtasks.filter((item) => item.isCompleted).length
-    );
-  };
-  useEffect(() => {
-    if (loading) {
-      const currentBoard = boardState.find(
-        (item) => item.name === settingState.activeBoard
-      );
-      const activatedColumn = currentBoard?.columns.find(
-        (item) => item.id === settingState.activateColumn
-      );
-      // const currentTask = currentBoard?.columns.
-      setActivatedTask(
-        activatedColumn?.tasks.find(
-          (item) => item.id === settingState.activateTask
-        )
-      );
-      setLoading(false);
-    }
-    // setActivatedTask(activatedTask);
-    // console.log(activatedTask?.title);
-    setCompletedTask(
-      activatedTask!.subtasks.filter((item) => item.isCompleted).length
+      currentTask?.subtasks.filter((item) => item.isCompleted).length
     );
   }, [
-    activatedTask,
+    currentTask,
     boardState,
     loading,
     settingState.activateColumn,
@@ -71,31 +97,48 @@ const ViewTaskModal: React.FC<ViewTaskModalProps> = ({ darkMode }) => {
   ]);
   useEffect(() => {
     if (modalsState.open) setLoading(true);
-  }, [modalsState]);
+    reset({ status: "" });
+  }, [modalsState, reset]);
+
   const toggleSubtask = (subTaskId: number) => {
-    const updatedSubtask = activatedTask?.subtasks.map((item) =>
+    const updatedSubtask = currentTask?.subtasks.map((item) =>
       item.id === subTaskId ? { ...item, isCompleted: !item.isCompleted } : item
     );
-    // console.log(updatedSubtask);
-    console.log(activatedTask?.subtasks);
-    console.log(updatedSubtask);
+    setBoardState((prev) => {
+      return prev.map((board) => {
+        if (board.name === settingState.activeBoard) {
+          let columns = board.columns;
+          const activatedColumn = columns.find(
+            (col) => col.id === settingState.activateColumn
+          );
+          let tasks = activatedColumn?.tasks;
+          const activatedTask = tasks?.find(
+            (task) => task.id === settingState.activateTask
+          );
+          const updatedTask = {
+            ...(activatedTask as TaskType),
+            status: getValues("status"),
+            subtasks: updatedSubtask as SubtasksType[],
+          };
+          console.log(updatedTask);
 
-    setActivatedTask((prev) => {
-      return { ...prev, subtasks: updatedSubtask };
+          tasks = tasks?.map((task) =>
+            task.id === settingState.activateTask ? updatedTask : task
+          );
+          columns = columns.map((col) =>
+            col.id === settingState.activateColumn
+              ? { ...col, tasks: tasks as TaskType[] }
+              : col
+          );
+
+          return { ...board, columns: columns };
+        }
+        return board;
+      });
     });
-    // setActivatedTask({
-    //   description: "",
-    //   id: 11,
-    //   status: "",
-    //   subtasks: updatedSubtask,
-    //   title: "",
-    // });
-    // console.log(tests);
-
-    // console.log(activatedTask?.subtasks[0]);
   };
 
-  const checkBox = activatedTask?.subtasks.map((item) => (
+  const checkBox = currentTask?.subtasks.map((item) => (
     <Checkbox
       key={item.id}
       darkMode={darkMode}
@@ -119,33 +162,43 @@ const ViewTaskModal: React.FC<ViewTaskModalProps> = ({ darkMode }) => {
          }
           p-[25px] 
           shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]
-          focus:outline-none`}
-      >
+          focus:outline-none`}>
         <Dialog.Title
-          className={` ${darkMode ? "text-white" : "text-black"} text-800`}
-        >
+          className={` ${darkMode ? "text-white" : "text-black"} text-800`}>
           <div className="flex items-center">
-            <p> {activatedTask?.title}</p>
+            <p> {currentTask?.title}</p>
           </div>
         </Dialog.Title>
         <Dialog.Description className="text-mediumGrey my-[1.5rem] text-500">
-          {activatedTask?.description}
+          {currentTask?.description}
         </Dialog.Description>
         <Dialog.Description
           className={` pb-4 text-500 ${
             darkMode ? "text-white" : "text-mediumGrey"
-          }`}
-        >
-          Subtasks ({completedTask} of {activatedTask?.subtasks.length})
+          }`}>
+          Subtasks ({completedTask} of {currentTask?.subtasks.length})
         </Dialog.Description>
         {checkBox}
         <p
           className={` text-400 pt-4 pb-2 ${
             darkMode ? "text-white" : "text-mediumGrey"
-          }`}
-        >
+          }`}>
           Current Status
         </p>
+        <Controller
+          control={control}
+          name="status"
+          defaultValue={columnsName[0]}
+          render={({ field: { onChange, value, ref } }) => (
+            <DropMenu
+              darkMode={darkMode}
+              onChange={onChange}
+              value={value}
+              columnsName={columnsName}
+              ref={ref}
+            />
+          )}
+        />
         <button onClick={sprawdz}>kkkkk</button>
       </Dialog.Content>
     </Dialog.Portal>
